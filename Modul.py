e8064087980e84d21bed79814ed1524e730953e8
ArDa0226 <161866557+ArDa0226@users.noreply.github.com>
2024-03-12 20:49:14 +0500

    Домашнее задание по теме: "Модули и пакеты"
    Создан Модуль с 2-мя функциями, выводящими 'Hello, world!'

---
+++ Modul.py
@@ -0,0 +1,9 @@
+
+def func1():
+    print('Hello, ')
+
+func1()
+def func2():
+    print('world!')
+
+func2()
---
+++ main.py
@@ -0,0 +1,17 @@
+
+import Modul
+from Modul import func1
+
+func1()
+
+from Modul import func2
+
+func2()
+
+from Modul import func1 as f1
+
+f1()
+
+from  Modul import func2 as f2
+
+f2()
